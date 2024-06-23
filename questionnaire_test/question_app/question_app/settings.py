0a0e30aa8ca5876d7b612c3deb3ab61510c668dd
ridge-f <rachel_feng@outlook.com>
2024-06-23 02:39:15 +0100

    settings update

--- questionnaire_test/question_app/question_app/settings.py
+++ questionnaire_test/question_app/question_app/settings.py
@@ -9,7 +9,8 @@ SECRET_KEY = 'your-secret-key'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-3-84-10-251.compute-1.amazonaws.com', 'localhost', '127.0.0.1']
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',

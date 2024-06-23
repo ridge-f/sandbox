a6529a7ed6d01b8a431d0020121c38cf1d465881
ridge-f <rachel_feng@outlook.com>
2024-06-23 02:45:01 +0100

    settings update

--- questionnaire_test/question_app/question_app/settings.py
+++ questionnaire_test/question_app/question_app/settings.py
@@ -37,7 +37,7 @@ ROOT_URLCONF = 'question_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'question_app', 'questions', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
